86bbad51b7c196890a4aec3df05e5304bc92e2de
Mateus Gonçalves Teteo <mateusgoncalvesteteo@gmail.com>
2026-06-16 11:02:50 -0300

    funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -12,15 +12,15 @@ void inicializarTabuleiro();
 void mostrarTabuleiro(char player);
 
 // ===============================
-// JOGO (PASSO 1)
+// JOGO (PASSO 1)//dividir em passos torna mais facil
 // ===============================
 bool moverPeca(int x1, int y1, int x2, int y2, char jogadorAtual);
 bool jogoTerminou();
 
 // ===============================
-// UTILITÁRIO (opcional mas recomendado)
+// UTILITÁRIO (futuro)
 // ===============================
-int contarPecas(char peca);
+int contarPecas(char peca);// mais pra frente eu faço (Mateus)
 // ===============================
 // LIMPAR TELA
 // ===============================
