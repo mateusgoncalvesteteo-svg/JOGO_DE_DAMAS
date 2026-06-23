f3eb29e986bdc68607df5926dc5a2b664f3b0c84
Mateus Gonçalves Teteo <mateusgoncalvesteteo@gmail.com>
2026-06-23 12:58:31 -0300

    adicionada funcoes de fim de jogo

--- funcoes.h
+++ funcoes.h
@@ -15,7 +15,10 @@ bool moverPeca(Jogo& jogo, int x1, int y1, int x2, int y2, char jogadorAtual);
 bool moverDama(Jogo& jogo, int x1, int y1, int x2, int y2, char jogadorAtual);
 bool existeCaptura(Jogo& jogo, int x, int y);
 bool jogadorTemCaptura(Jogo& jogo, char jogadorAtual);
-int contarPecas(Jogo& jogo, char jogador);// <-- contar pecas
+int contarPecas(Jogo& jogo, char jogador);
+bool jogadorTemMovimentos(Jogo& jogo, char jogador);//complementar de ganhador
+bool verificarFimDeJogo(Jogo& jogo,char jogadorAtual,bool desistiu);//complementar de ganhador
+char verificarVencedor(Jogo& jogo,char jogadorAtual,bool desistiu);//<-- adiconado verificar ganhador
 void limparTela();
 int lerCoordenada();
 
