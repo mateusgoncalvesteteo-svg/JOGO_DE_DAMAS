36b99557d326d99ed0f4ed4a9452154c93ee17ae
Pablo Adriano Dias Nôga Alves <pablo.alves.125@ufrn.edu.br>
2026-06-23 20:45:23 -0300

    Enhance funcoes.h with player profile management
    Added player profile structure and functions for profile management.

--- funcoes.h
+++ funcoes.h
@@ -1,9 +1,20 @@
 #ifndef FUNCOES_H
 #define FUNCOES_H
 
-// Criamos a Struct um dos pedidos do prof
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Struct jogo
 struct Jogo {
     char tabuleiro[8][8];
+};
+
+// Perfil do Jogador
+struct PerfilJogador {
+    string nome;
+    int vitorias;
 };
 
 // Declaração das funções
@@ -16,10 +27,9 @@ bool moverDama(Jogo& jogo, int x1, int y1, int x2, int y2, char jogadorAtual);
 bool existeCaptura(Jogo& jogo, int x, int y);
 bool jogadorTemCaptura(Jogo& jogo, char jogadorAtual);
 int contarPecas(Jogo& jogo, char jogador);
-bool jogadorTemMovimentos(Jogo& jogo, char jogador);//complementar de ganhador
-bool verificarFimDeJogo(Jogo& jogo,char jogadorAtual,bool desistiu);//complementar de ganhador
-char verificarVencedor(Jogo& jogo,char jogadorAtual,bool desistiu);//<-- adiconado verificar ganhador
 void limparTela();
 int lerCoordenada();
+int buscaRecursiva(const vector<PerfilJogador>& perfis, string nomeBuscado, int indiceAtual);
+void atualizarPerfil(vector<PerfilJogador>& perfis, string nomeJogador);
 
 #endif
