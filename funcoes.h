5b93a0337a6b342e6569e60b090cf7b7b6b45839
Mateus Gonçalves Teteo <mateusgoncalvesteteo@gmail.com>
2026-06-24 07:56:57 -0300

    correcoes

--- funcoes.h
+++ funcoes.h
@@ -1,17 +1,16 @@
 #ifndef FUNCOES_H
 #define FUNCOES_H
-
-#include <string>
 #include <vector>
+#include <string>      
 
 using namespace std;
 
-// Struct jogo
+// Struct do Jogo
 struct Jogo {
     char tabuleiro[8][8];
 };
 
-// Perfil do Jogador
+// Struct do Perfil
 struct PerfilJogador {
     string nome;
     int vitorias;
@@ -27,9 +26,12 @@ bool moverDama(Jogo& jogo, int x1, int y1, int x2, int y2, char jogadorAtual);
 bool existeCaptura(Jogo& jogo, int x, int y);
 bool jogadorTemCaptura(Jogo& jogo, char jogadorAtual);
 int contarPecas(Jogo& jogo, char jogador);
-void limparTela();
-int lerCoordenada();
+bool jogadorTemMovimentos(Jogo& jogo, char jogador);
+bool verificarFimDeJogo(Jogo& jogo, char jogadorAtual, bool desistiu);
+char verificarVencedor(Jogo& jogo, char jogadorAtual, bool desistiu);
 int buscaRecursiva(const vector<PerfilJogador>& perfis, string nomeBuscado, int indiceAtual);
 void atualizarPerfil(vector<PerfilJogador>& perfis, string nomeJogador);
+void limparTela();
+int lerCoordenada();
 
 #endif
