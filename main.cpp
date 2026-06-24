cb36bf678324b56016d5f0581d45c8e0ee21f0c4
Mateus Gonçalves Teteo <mateusgoncalvesteteo@gmail.com>
2026-06-24 09:41:43 -0300

    adicionado perfis e melhoras na main

--- main.cpp
+++ main.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 #include "funcoes.h"
-
 using namespace std;
 
 int main() {
-    // Chama o menu
-    char opcaoEscolhida = menu();
-
-    // Se o usuário digitou 1, inicia o jogo
-    switch(opcaoEscolhida)
-{
-    case '1':
-        jogar();
-        break;
-
-    case '0':
-        cout << "\nEncerrando jogo...\n";
-        break;
-    }
-
+    vector<PerfilJogador> perfis = carregarPerfis(); // Carregar ranking
+    
+    char opcaoEscolhida;
+    
+    do {
+        opcaoEscolhida = menu();
+        
+        switch(opcaoEscolhida)
+        {
+            case '1':
+                jogar(perfis);
+                perfis = carregarPerfis(); // Recarregar após partida
+                salvarPerfis(perfis);
+                break;
+                
+            case '2':
+                exibirRanking(perfis);
+                break;
+                
+            case '0':
+                cout << "\nEncerrando jogo...\n";
+                break;
+        }
+        
+    } while(opcaoEscolhida != '0');
+    
     cout << "Obrigado por jogar!\n";
     return 0;
 }
